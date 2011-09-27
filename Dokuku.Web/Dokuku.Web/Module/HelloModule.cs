d1bbb643f59ecb4ce557f82d2f0f6371b2541085
unknown <Fatur@.(none)>
2011-09-28 05:10:44 +0700

    new project dokuku

---
+++ Dokuku.Web/Dokuku.Web/Module/HelloModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Dokuku.Web.Module
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+            Get["/hello/{name}"] = parameters =>
+            {
+                return "Hello " + parameters.name;
+            };
+
+        }
+    }
+}
